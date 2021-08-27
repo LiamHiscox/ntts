d0a5fb33f93641a29775ddf6b19ff6565235275f
Liam Hiscox <liamhiscox@gmx.at>
2021-08-27 14:22:50 +0200

    #21 get the versions of packages and calculate the closest out of a given list

---
+++ lib/dependency-installer/version-handler/version-handler.ts
@@ -0,0 +1,39 @@
+import {ScriptRunner} from "../../script-runner/script-runner";
+
+export type PackageVersion = string;
+
+interface PackageVersionModel {
+    dependencies: {
+        [packageName: string]: {
+            version: PackageVersion
+        }
+    }
+}
+
+export class VersionHandler {
+    /**
+     * @param packageName the package to get the available version of
+     * @returns Array<PackageVersion> a list of all the available version of the given package
+     */
+    static packageVersions = (packageName: string): Array<PackageVersion> => {
+        return ScriptRunner.runParsed<Array<PackageVersion>>(`npm view ${packageName} versions --json`);
+    }
+
+    /**
+     * @param packageName the name of the installed package
+     * @returns PackageVersion the installed version of the given package
+     */
+    static packageVersion = (packageName: string): PackageVersion => {
+        return ScriptRunner
+            .runParsed<PackageVersionModel>(`npm ls ${packageName} --json --depth`)
+            .dependencies[packageName]
+            .version;
+    }
+
+    /**
+     * @returns PackageVersion the installed version of Node.js
+     */
+    static nodeVersion = (): PackageVersion => {
+        return ScriptRunner.run('node --version').trim().substring(1);
+    }
+}
--- lib/script-runner/script-runner.ts
+++ lib/script-runner/script-runner.ts
@@ -1,12 +1,26 @@
-import {execSync, ExecSyncOptionsWithBufferEncoding} from "child_process";
+import {execSync, StdioOptions} from "child_process";
 
 export class ScriptRunner {
     /**
      * @param script the script to run in the terminal.
-     * @param options the options for execSync function.
-     * @returns the stdout as a buffer from the given script.
+     * @param stdio how the stdio should be handled.
+     * @returns string the stdout from the given script.
      */
-    static run = (script: string, options?: ExecSyncOptionsWithBufferEncoding): Buffer => {
-        return execSync(script, { stdio: 'inherit',  ...options });
+    static run(script: string, stdio?: StdioOptions): string {
+        return execSync(script, { stdio, encoding: 'utf8' });
+    }
+
+    /**
+     * @param script the script to run in the terminal.
+     * @param stdio how the stdio should be handled.
+     * @returns T stdout as an object from the given script.
+     */
+    static runParsed<T>(script: string, stdio?: StdioOptions): T {
+        const result = execSync(script, { stdio, encoding: 'utf8' });
+        return this.parseResponse<T>(result);
+    }
+
+    private static parseResponse<T>(response: string): T {
+        return JSON.parse(response) as T;
     }
 }
