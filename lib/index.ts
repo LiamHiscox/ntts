72610808ba9cb016727541230dada8ddea137686
Liam Hiscox <liamhiscox@gmx.at>
2021-10-13 15:25:29 +0200

    #49 fixed failing tests

--- lib/index.ts
+++ lib/index.ts
@@ -5,7 +5,7 @@ import {TsconfigHandler} from "./tsconfig-handler/tsconfig-handler";
 import {PackageJsonHandler} from "./package-json-handler/package-json-handler";
 import {InputValidator} from "./input-validator/input-validator";
 import {PackageManager} from "./models/package-manager";
-import {IgnoreConfigParser} from "./file-rename/ignore-config-parser/ignore-config-parser";
+import {IgnoreConfigParser} from "./ignore-config-parser/ignore-config-parser";
 import {CodeRefactor} from "./code-refactor/code-refactor";
 import {Project} from "ts-morph";
 
--- tests/file-rename.test.ts
+++ tests/file-rename.test.ts
@@ -2,6 +2,7 @@ import {FileRename} from "../lib/file-rename/file-rename";
 import * as fse from "fs-extra";
 import {writeFileSync} from "fs";
 import globby from "globby";
+import {IgnoreConfigParser} from "../lib/file-rename/ignore-config-parser/ignore-config-parser";
 
 const sampleCopy = 'tests/sample-copy';
 const sample = 'tests/sample';
@@ -18,6 +19,7 @@ afterEach(() => {
 });
 
 test('should rename a single file', () => {
+  const ignores = IgnoreConfigParser.getIgnores();
   FileRename.rename('src', ignores);
   expect(globby.sync(["**/*.ts"]).sort())
     .toEqual(["src/index.ts"].sort());
@@ -25,12 +27,14 @@ test('should rename a single file', () => {
 
 test('should not rename config file', () => {
   writeFileSync('.gitignore', '*.config.js');
+  const ignores = IgnoreConfigParser.getIgnores();
   FileRename.rename('.', ignores);
   expect(globby.sync(["**/*.ts"]).sort())
     .toEqual(["src/index.ts", "js-ts.ts"].sort());
 });
 
 test('should rename all files recursively', () => {
+  const ignores = IgnoreConfigParser.getIgnores();
   FileRename.rename('.', ignores);
   expect(globby.sync(["**/*.ts"]).sort())
     .toEqual(["src/index.ts", "js-ts.ts", "babel.config.ts"].sort());
