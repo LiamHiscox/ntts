16fe401586fb1334229a3fd2c93a2d14efde2223
Liam Hiscox <liamhiscox@gmx.at>
2021-11-02 21:21:06 +0100

    #44 empty requires are refactored to empty import

--- tests/imports-refactor/expression-import.test.ts
+++ tests/imports-refactor/expression-import.test.ts
@@ -6,5 +6,11 @@ const project = new Project();
 test('should refactor expression statement require', () => {
   const sourceFile = project.createSourceFile('standard-require.ts', 'require("ts-morph");', {overwrite: true});
   ImportsRefactor.requiresToImports(sourceFile);
-  expect(sourceFile.getText()).toEqual('import ts_morph from "ts-morph";');
+  expect(sourceFile.getText()).toEqual('import "ts-morph";');
+});
+
+test('should not add empty import if import already exists', () => {
+  const sourceFile = project.createSourceFile('standard-require.ts', 'require("ts-morph");', {overwrite: true});
+  ImportsRefactor.requiresToImports(sourceFile);
+  expect(sourceFile.getText()).toEqual('import "ts-morph";');
 });
