a0e158a304af97a0b97abf9c01477e10f20a8a92
Liam Hiscox <liamhiscox@gmx.at>
2021-11-16 21:05:20 +0100

    #78 fixed failing tests

--- tests/script-runner.test.ts
+++ tests/script-runner.test.ts
@@ -1,13 +1,14 @@
 import {ScriptRunner} from "../lib/script-runner/script-runner";
 
-test('should pipe simple script', () => {
-    expect(ScriptRunner.runPipe('npm --version')).toMatch(/^\d+\.\d+\.\d+$/);
+test('should pipe simple script', async () => {
+    expect(await ScriptRunner.runPipe('npm --version')).toMatch(/^\d+\.\d+\.\d+$/);
 });
 
 test('should inherit simple script', () => {
-    expect(() => ScriptRunner.runIgnore('npm --version')).not.toThrow();
+    expect(async () => await ScriptRunner.runIgnore('npm --version')).not.toThrow();
 });
 
-test('should run script parsed result', () => {
-    expect(ScriptRunner.runParsed<{name: string}>('npm ls --json --depth').name).toBe("nodejs-to-ts");
+test('should run script parsed result', async () => {
+    const result = await ScriptRunner.runParsed<{name: string}>('npm ls --json --depth');
+    expect(result.name).toBe("nodejs-to-ts");
 });
